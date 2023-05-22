f07a2427612dd90ef1c86545e47b303ab617b040
kritsanapr <kritsana.pr.41@gmail.com>
2023-05-22 12:35:24 +0700

    add constant config file.

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -1,17 +1,22 @@
 // import { Button, Stack } from '@mui/material'
-import { Delete, Send, Photo } from '@mui/icons-material'
+import { Delete, Send, Photo } from "@mui/icons-material";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import AuthLayout from "./layouts/AuthLayout"
-import Login from "./pages/Login"
-import BackendLayout from "./layouts/BackendLayout"
-import Dashboard from "./pages/Dashboard"
-import Product from "./pages/Product"
-import Report from "./pages/Report"
-import Setting from "./pages/Setting"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/Login";
+import BackendLayout from "./layouts/BackendLayout";
+import Dashboard from "./pages/Dashboard";
+import Product from "./pages/Product";
+import Report from "./pages/Report";
+import Setting from "./pages/Setting";
+import {
+  DASHBOARD_PATH,
+  PRODUCT_PATH,
+  REPORT_PATH,
+  SETTING_PATH,
+} from "./config/contants";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
@@ -19,15 +24,14 @@ function App() {
           <Route path="/" element={<Login />} />
         </Route>
         <Route element={<BackendLayout />}>
-          <Route path="/backend/dashboard" element={<Dashboard />} />
-          <Route path="/backend/product" element={<Product />} />
-          <Route path="/backend/report" element={<Report />} />
-          <Route path="/backend/setting" element={<Setting />} />
+          <Route path={DASHBOARD_PATH} element={<Dashboard />} />
+          <Route path={PRODUCT_PATH} element={<Product />} />
+          <Route path={REPORT_PATH} element={<Report />} />
+          <Route path={SETTING_PATH} element={<Setting />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
-
+  );
 }
 
-export default App
+export default App;
---
+++ reactapp/src/config/contants.ts
@@ -0,0 +1,20 @@
+// System Constants
+export const SYSTEM_NAME = 'React App';
+
+
+// Backend Page Title
+export const DASHBOARD_TITLE = 'Dashboard';
+export const PRODUCT_TITLE = 'Product';
+export const REPORT_TITLE = 'Report';
+export const SETTING_TITLE = 'Setting';
+
+// Backend Route URL
+const BASE_PATH = '/backend/';
+export const DASHBOARD_PATH = BASE_PATH + 'dashboard';
+export const PRODUCT_PATH = BASE_PATH + 'product';
+export const REPORT_PATH = BASE_PATH + 'report';
+export const SETTING_PATH = BASE_PATH + 'setting';
+
+// Profile Name and Role
+export const PROFILE_NAME = 'Kritsana Prasit';  
+export const PROFILE_ROLE = 'Administrator';
