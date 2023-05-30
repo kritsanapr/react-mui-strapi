e150e26cc86f9438d443e0efa04935b7dad5ae5e
kritsanapr <kritsana.pr.41@gmail.com>
2023-05-30 16:09:34 +0700

    feat : add authUserAPI service.

---
+++ reactapp/src/services/authUserAPI.ts
@@ -0,0 +1,19 @@
+import Axios from "axios";
+
+const authUserAPI = async (data: any) => {
+    try {
+        return Axios.post('/auth/local', data, {
+            baseURL: import.meta.env.VITE_BASE_URL_API,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error in authUserAPI');
+    }
+}
+
+
+export default { authUserAPI }
